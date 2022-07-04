78597effb6045e6b076d24d12e936c2812fa4f40
lauter <chenqintao@salmonads.com>
2022-07-04 21:43:34 +0800

    Initial Commit

---
+++ Phantom/PhantomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhantomApp.swift
+//  Phantom
+//
+//  Created by 陈钦涛 on 2022/7/4.
+//
+
+import SwiftUI
+
+@main
+struct PhantomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
